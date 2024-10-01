178d316db16e6fc40d9e4a79fc402e3974c0ff12
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-09-30 21:00:09 -0400

    Fixing statistics scraper for faster and efficient scraping

--- statistics.py
+++ statistics.py
@@ -1,30 +1,38 @@
 import re
 from db import get_client
 from datetime import datetime
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
 
 def run(playwright: Playwright) -> None:
     db = get_client()
     courses = list(db["courses"].find({ "finalized": True }, projection={ "_id": False }))
 
-    browser = playwright.chromium.launch(headless=True)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
-    for course in courses:
-        page.goto(f"https://uwflow.com/course/{course['code'].replace(' ', '').lower()}")
-        page.get_by_text("%liked").click()
-        elements = page.locator("div").filter(has_text=re.compile(r"^[0-9]{1,}%$")).all()
+    current_date = datetime.now().strftime("%d/%m/%Y")
 
-        statistics = [re.findall(r"[0-9]{1,}", element.text_content())[0] for element in elements]
-        if len(statistics) != 5:
+    for course in courses:
+        if "statistics" in course and "lastUpdated" in course["statistics"] and course["statistics"]["lastUpdated"] == current_date:
+            print(f"{course['code']} already has been updated with UWFlow statistics as of {course['statistics']['lastUpdated']}")
+            continue
+
+        page.goto(f"https://uwflow.com/course/{course['code'].replace(' ', '').lower()}")
+        page.get_by_text("liked").first.click()
+        elements = page.locator("div").filter(has_text=re.compile(r"^(?:([0-9]{1,}%)|(N/A))$")).all()
+
+        stats = [re.findall(r"(?:(?:[0-9]{1,})|(?:N/A))", element.text_content())[0] for element in elements]
+        if len(stats) != 5:
             print(f"UWFlow statistics not found/incomplete for {course['code']}.")
             continue
-        statistics.pop(1) # Duplicate value for easy percentage
-        statistics.pop(3) # Duplicate value for liked percentage
-        statistics_object = { "liked": statistics[0], "easy": statistics[1], "useful": statistics[2], "lastUpdated": datetime.now().strftime("%d/%m/%Y") }
+        stats.pop(1) # Duplicate value for easy percentage
+        stats.pop(3) # Duplicate value for liked percentage
+        current_date = datetime.now().strftime("%d/%m/%Y")
+        stats_object = { "liked": stats[0], "easy": stats[1], "useful": stats[2], "lastUpdated": current_date }
         
-        db["courses"].find_one_and_update({ "finalized": True, "code": course["code"] }, { "$set": { "statistics": statistics_object } }, projection={ "_id": False })
+
+        db["courses"].find_one_and_update({ "finalized": True, "code": course["code"] }, { "$set": { "statistics": stats_object } }, projection={ "_id": False })
         print(f"Updated {course['code']} with UWFlow statistics.")
 
     context.close()
